431228e4267550e4c8c834962ca1abc60c57f39a
suryanshp1 <suryanshp1@gmail.com>
2026-03-02 15:36:45 +0530

    feat: next js config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: 'export',
+  basePath: isProd ? "/newsletter" : "",
+  assetPrefix: isProd ? "/newsletter" : "",
   images: {
     unoptimized: true
   }
